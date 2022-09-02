0f4b0b95149e276112ab34746c9f76c6e5c4de9a
aliaa.a2 <aliaa.a2@P-0G-0B111-0429>
2022-09-02 12:40:03 +0200

    Projektdateien hinzufügen.

---
+++ aufgaben/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string a = "Hallo";
+string b = "H";
+
+b += "allo";
+Console.WriteLine(b);  //
+Console.WriteLine(b.Length);
+Console.WriteLine(a==b);
+Console.WriteLine((object)a == b);
+
+
+
+
+//double x = 2.0;
+//double y = x * x + 1 + x / 4;
+//Console.WriteLine(y);
+//Ausgabe: "5,5";
+
+
+//double x = 2.0;
+//double y = x * (x + 1 + x / 4);
+//Console.WriteLine(y);
+//Ausgabe: "7";
+
+//bool b = true && false || 5 > 6;
+//Console.WriteLine(b);
+// Ausgabe:"false"
+
+
+int z = 50;
+bool numeric = z > 47 && z < 58;
+Console.WriteLine(numeric);
+//true
+
